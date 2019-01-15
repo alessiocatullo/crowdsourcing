dc8bd98c44a788f47fb255b0bfbe5e929f312d01
CATULLO Alessio <alessio.catullo@soprasteria.com>
2019-01-15 22:03:09 +0100

    fixed countAns

--- DB/FUNCTION/countAnswer.sql
+++ DB/FUNCTION/countAnswer.sql
@@ -1,6 +1,8 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `PROPORTION_RESULT`(tot INT, value INT) RETURNS int(11)
+CREATE DEFINER=`root`@`localhost` FUNCTION `COUNTANSWER`(task_in INT, answer_in INT) RETURNS int(11)
 BEGIN
-	DECLARE n INT;
-    SET n = value*100/tot;
-RETURN n;
+	DECLARE nof_answer INT;
+    
+    SET nof_answer = (SELECT COUNT(*) FROM task_performed WHERE task = task_in AND answer = answer_in); 
+    
+RETURN nof_answer;
 END
